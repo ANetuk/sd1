57b47f85c9e25db3b123518d96ecdc2bbda466f6
Artem Netuk <netukartem@ya.ru>
2026-05-12 09:24:40 +0300

    Сделал задачу по работе с датой и временем

---
+++ Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Task8;
+
+class Program
+{
+    public static void Main()
+    {
+        const string dateTimeIsoString = "2026-05-12T06:20:00.0000000Z";
+        try
+        {
+            var dateTime = DateTimeOffset.ParseExact(dateTimeIsoString, "O", CultureInfo.InvariantCulture);
+            Console.WriteLine("Date: " + dateTime);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
